d4e798636dfeb08c1940363be489355ea1cb10b0
Nino <n.van.der.laan@amsterdam.nl>
2024-07-31 14:58:02 +0200

    merge fixes

--- app/apps/cases/models.py
+++ app/apps/cases/models.py
@@ -1,33 +1,11 @@
 from django.db import models
 
 from .tasks import task_create_case
-<<<<<<< HEAD
-
-# class Address(models.Model):
-#     street = models.CharField(max_length=200)
-#     city = models.CharField(max_length=100)
-#     state = models.CharField(max_length=100)
-#     postal_code = models.CharField(max_length=20)
-
-
-# class Vve(models.Model):
-#     vve_statutaire_naam = models.TextField()
 
 
 
-=======
->>>>>>> 2ba300e (add appinsights)
 class Case(models.Model):
-<<<<<<< HEAD
-    description = models.TextField()
-
-=======
     description = models.TextField(null=True)
-<<<<<<< HEAD
-    # housing_association = models.ForeignKey(HousingAssociation, on_delete=models.CASCADE, related_name='cases')
->>>>>>> 6a896e0 (wip)
-=======
->>>>>>> 2ba300e (add appinsights)
     def save(self, *args, **kwargs):
         task_create_case.delay(self.description)
         super().save(*args, **kwargs)
