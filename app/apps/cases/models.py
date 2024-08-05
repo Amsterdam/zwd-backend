deb4a78fe8385024dda20249f6a7c1d62b8348f8
Nino <n.van.der.laan@amsterdam.nl>
2024-08-05 10:52:30 +0200

    rm dependency

--- app/apps/cases/models.py
+++ app/apps/cases/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
-from .tasks import task_create_case
-
 
 class Case(models.Model):
     description = models.TextField(null=True)
-
-    def save(self, *args, **kwargs):
-        task_create_case.delay(self.description)
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Case: {self.id}"
