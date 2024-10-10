85e705262ad64beafacf5ea78528aecc7cd60602
Nino <n.van.der.laan@amsterdam.nl>
2024-10-10 15:35:56 +0200

    fix duplicates

--- app/apps/homeownerassociation/models.py
+++ app/apps/homeownerassociation/models.py
@@ -17,9 +17,13 @@ class HomeownerAssociation(models.Model):
             return existing_hoa
 
         hoa_response = client.get_hoa_by_name(hoa_name)
+        distinct_hoa_response = list(
+            {hoa["votIdentificatie"]: hoa for hoa in hoa_response}.values()
+        )
+        # print(distinct_hoa_response)
         model = HomeownerAssociation.objects.create(
             name=hoa_name,
-            build_year=hoa_response[0].get("pndOorspronkelijkBouwjaar"),
-            number_of_appartments=len(hoa_response),
+            build_year=distinct_hoa_response[0].get("pndOorspronkelijkBouwjaar"),
+            number_of_appartments=len(distinct_hoa_response),
         )
         return model
--- app/apps/homeownerassociation/tests/tests_models.py
+++ app/apps/homeownerassociation/tests/tests_models.py
@@ -24,27 +24,6 @@ class HomeownerAssociationModelTest(TestCase):
         mock_client.get_hoa_name_by_bag_id.assert_called_once_with("some_bag_id")
 
     @patch("apps.homeownerassociation.models.DsoClient")
-    def test_get_or_create_hoa_by_bag_id_new_hoa(self, MockDsoClient):
-        # Mock the DsoClient and its methods
-        mock_client = MockDsoClient.return_value
-        mock_client.get_hoa_name_by_bag_id.return_value = "New HOA"
-        mock_client.get_hoa_by_name.return_value = [
-            {"pndOorspronkelijkBouwjaar": 2010},
-            {"pndOorspronkelijkBouwjaar": 2010},
-        ]
-
-        # Call the method
-        result = HomeownerAssociation.get_or_create_hoa_by_bag_id("some_bag_id")
-
-        # Assert a new HOA is created
-        self.assertIsInstance(result, HomeownerAssociation)
-        self.assertEqual(result.name, "New HOA")
-        self.assertEqual(result.build_year, 2010)
-        self.assertEqual(result.number_of_appartments, 2)
-        mock_client.get_hoa_name_by_bag_id.assert_called_once_with("some_bag_id")
-        mock_client.get_hoa_by_name.assert_called_once_with("New HOA")
-
-    @patch("apps.homeownerassociation.models.DsoClient")
     def test_get_or_create_hoa_by_bag_id_existing_hoa_no_new_hoa(self, MockDsoClient):
         # Mock the DsoClient and its methods
         mock_client = MockDsoClient.return_value
@@ -62,3 +41,41 @@ class HomeownerAssociationModelTest(TestCase):
         self.assertEqual(result, existing_hoa)
         mock_client.get_hoa_name_by_bag_id.assert_called_once_with("some_bag_id")
         mock_client.get_hoa_by_name.assert_not_called()
+
+    @patch("apps.homeownerassociation.models.DsoClient")
+    def test_get_or_create_hoa_by_bag_id_new_hoa(self, MockDsoClient):
+        # Mock the DsoClient and its methods
+        mock_client = MockDsoClient.return_value
+        mock_client.get_hoa_name_by_bag_id.return_value = "New HOA"
+        mock_client.get_hoa_by_name.return_value = [
+            {"pndOorspronkelijkBouwjaar": 2010, "votIdentificatie": "123"},
+            {"pndOorspronkelijkBouwjaar": 2010, "votIdentificatie": "123"},
+        ]
+
+        # Call the method
+        result = HomeownerAssociation.get_or_create_hoa_by_bag_id("some_bag_id")
+        # Assert a new HOA is created
+        self.assertIsInstance(result, HomeownerAssociation)
+        self.assertEqual(result.name, "New HOA")
+        self.assertEqual(result.build_year, 2010)
+        self.assertEqual(result.number_of_appartments, 1)
+        mock_client.get_hoa_name_by_bag_id.assert_called_once_with("some_bag_id")
+        mock_client.get_hoa_by_name.assert_called_once_with("New HOA")
+
+    # write a test to verify that duplicate appartements are only counted once
+    @patch("apps.homeownerassociation.models.DsoClient")
+    def test_get_or_create_hoa_by_bag_id_new_hoa_duplicate_appartments(
+        self, MockDsoClient
+    ):
+        mock_client = MockDsoClient.return_value
+        mock_client.get_hoa_name_by_bag_id.return_value = "HOA"
+        mock_client.get_hoa_by_name.return_value = [
+            {"pndOorspronkelijkBouwjaar": 2010, "votIdentificatie": "333"},
+            {"pndOorspronkelijkBouwjaar": 2010, "votIdentificatie": "333"},
+            {"pndOorspronkelijkBouwjaar": 2010, "votIdentificatie": "444"},
+        ]
+        result = HomeownerAssociation.get_or_create_hoa_by_bag_id("unique_id")
+        self.assertIsInstance(result, HomeownerAssociation)
+        self.assertEqual(result.name, "HOA")
+        self.assertEqual(result.build_year, 2010)
+        self.assertEqual(result.number_of_appartments, 2)
