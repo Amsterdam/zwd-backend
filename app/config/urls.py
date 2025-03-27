7e57867dc4a3f3a211ee5cd12ddcec2c5362028f
Nino <n.van.der.laan@amsterdam.nl>
2025-03-27 15:45:11 +0100

    wip admin entra auth

--- app/config/urls.py
+++ app/config/urls.py
@@ -24,6 +24,15 @@ router.register(
     r"homeowner-association", HomeOwnerAssociationView, basename="homeownerassociation"
 )
 
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from mozilla_django_oidc.views import OIDCAuthenticationCallbackView
+
+
+@login_required
+def admin_redirect(request):
+    return redirect("/admin")
+
 
 def ok(request):
     return HttpResponse("OK", status=200)
@@ -31,7 +40,9 @@ def ok(request):
 
 urlpatterns = [
     path("startup/", ok),
-    path("", ok),  #
+    path("", OIDCAuthenticationCallbackView.as_view(), name="login"),
+    path("oidc/", include("mozilla_django_oidc.urls")),
+    path("admin/login/", admin_redirect),
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
     path("data-model/", include("django_spaghetti.urls")),
