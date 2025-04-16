c479e0e19c3e4fcfb45ad0075ca119c6c9af0aa0
Nino <n.van.der.laan@amsterdam.nl>
2025-04-16 09:51:34 +0200

    rm print

--- app/apps/users/admin.py
+++ app/apps/users/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-print("CustomUserAdmin loaded")
-
 admin.site.unregister(User)
 
 
