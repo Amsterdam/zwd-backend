83b2541e619797ced4eaa0c6fa38d077acc0c06c
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-06-03 16:39:44 +0200

    WON-223 Refactor CaseEvent admin registration to use decorator and improve readability

--- app/apps/events/admin.py
+++ app/apps/events/admin.py
@@ -3,21 +3,28 @@ from django.contrib import admin
 from apps.events.models import CaseEvent
 
 
-admin.site.register(
-    CaseEvent,
-    admin.ModelAdmin,
-    readonly_fields=("created", "event_values"),
-    list_display=(
+@admin.register(CaseEvent)
+class CaseEventAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "event_values")
+    list_display = (
         "id",
         "emitter",
         "emitter_id",
         "emitter_type",
         "type",
+        "description",
         "created",
-    ),
-    list_filter=(
+    )
+    list_filter = (
         "created",
         "type",
-    ),
-    search_fields=("emitter_id",),
-)
+    )
+    search_fields = (
+        "emitter_id",
+        "case__id",
+    )
+
+    def description(self, obj):
+        return obj.event_values.get("description", "")
+
+    description.short_description = "Description"
