7aa109b496e20e26e971dc6994764c909ab818d8
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-08-07 16:09:24 +0200

    Add post_save signal to update Case.updated timestamp on task completion

--- app/apps/workflow/signals.py
+++ app/apps/workflow/signals.py
@@ -1,7 +1,9 @@
-from apps.workflow.models import CaseWorkflow
-from django.db.models.signals import pre_save
+from apps.cases.models import Case
+from apps.workflow.models import CaseWorkflow, GenericCompletedTask
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .utils import get_latest_version_from_config
+from django.utils import timezone
 
 
 @receiver(pre_save, sender=CaseWorkflow, dispatch_uid="case_workflow_pre_save")
@@ -30,3 +32,13 @@ def case_workflow_pre_save(sender, instance, **kwargs):
             instance.workflow_type,
             existing_main_workflow.workflow_version if existing_main_workflow else None,
         )
+
+
+# Updated Case.updated field if task is completed / generic task is created
+@receiver(
+    post_save,
+    sender=GenericCompletedTask,
+    dispatch_uid="update_case_updated_timestamp_on_task_complete",
+)
+def update_case_updated_timestamp_on_task_complete(sender, instance, created, **kwargs):
+    Case.objects.filter(id=instance.case_id).update(updated=timezone.now())
