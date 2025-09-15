f76992d750ba0ed24435a9ea86b7ecb6e57e4cab
Remy van der Wereld <r.van.der.wereld@amsterdam.nl>
2025-09-15 17:15:03 +0200

    Increase max page size limit to 1000 in CustomPagination

--- app/utils/pagination.py
+++ app/utils/pagination.py
@@ -4,4 +4,4 @@ from rest_framework.pagination import PageNumberPagination
 class CustomPagination(PageNumberPagination):
     page_size = 10  # Default page size
     page_size_query_param = "page_size"  # Allows clients to set page size
-    max_page_size = 100  # Maximum limit to prevent overload
+    max_page_size = 1000  # Maximum limit to prevent overload
