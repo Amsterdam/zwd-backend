3e5161d4aba197b92514edc86de0b2d23f1454b9
Koen Schaft <koen@schoenkaft.com>
2025-10-07 13:53:51 +0200

    Add additional Case API and model tests

--- app/apps/cases/tests/tests_models.py
+++ app/apps/cases/tests/tests_models.py
@@ -1,5 +1,5 @@
 from apps.homeownerassociation.models import HomeownerAssociation
-from apps.cases.models import AdviceType, Case
+from apps.cases.models import AdviceType, ApplicationType, Case
 from django.core import management
 from django.test import TestCase
 from model_bakery import baker
@@ -34,3 +34,87 @@ class CaseModelTest(TestCase):
         self.assertEqual(Case.objects.filter(end_date__isnull=False).count(), 1)
         self.assertEqual(Case.objects.filter(end_date__isnull=True).count(), 0)
         self.assertEqual(CaseWorkflow.objects.count(), 0)
+
+    def test_prefixed_dossier_id_for_course(self):
+        """Test that Course cases get CUR prefix"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=13
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.COURSE.value,
+            advice_type=None,
+        )
+        expected_prefix = f"{case.id}CUR"
+        self.assertEqual(case.prefixed_dossier_id, expected_prefix)
+
+    def test_prefixed_dossier_id_for_activation_team(self):
+        """Test that ActivationTeam cases get ACT prefix"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=13
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.ACTIVATIONTEAM.value,
+            advice_type=None,
+        )
+        expected_prefix = f"{case.id}ACT"
+        self.assertEqual(case.prefixed_dossier_id, expected_prefix)
+
+    def test_prefixed_dossier_id_for_hbo(self):
+        """Test that HBO cases get HBO prefix"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=13
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.ADVICE.value,
+            advice_type=AdviceType.HBO.value,
+        )
+        expected_prefix = f"{case.id}HBO"
+        self.assertEqual(case.prefixed_dossier_id, expected_prefix)
+
+    def test_prefixed_dossier_id_for_energy_advice_small_hoa(self):
+        """Test that Energy Advice for small HOA gets EAK prefix"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=10
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.ADVICE.value,
+            advice_type=AdviceType.ENERGY_ADVICE.value,
+        )
+        expected_prefix = f"{case.id}EAK"
+        self.assertEqual(case.prefixed_dossier_id, expected_prefix)
+
+    def test_prefixed_dossier_id_for_energy_advice_large_hoa(self):
+        """Test that Energy Advice for large HOA gets EAG prefix"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=20
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.ADVICE.value,
+            advice_type=AdviceType.ENERGY_ADVICE.value,
+        )
+        expected_prefix = f"{case.id}EAG"
+        self.assertEqual(case.prefixed_dossier_id, expected_prefix)
+
+    def test_course_case_has_no_advice_type(self):
+        """Test that Course cases should not have advice_type set"""
+        homeowner_association = baker.make(
+            HomeownerAssociation, number_of_apartments=13
+        )
+        case = baker.make(
+            Case,
+            homeowner_association=homeowner_association,
+            application_type=ApplicationType.COURSE.value,
+            advice_type=None,
+        )
+        self.assertIsNone(case.advice_type)
+        self.assertEqual(case.application_type, ApplicationType.COURSE.value)
