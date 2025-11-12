f89fa06443da1294b087000b1a1ca186c7679ba1
Koen Schaft <koen@schoenkaft.com>
2025-11-12 13:37:33 +0100

    Add is_active flag to HOA Contact

--- app/apps/homeownerassociation/models.py
+++ app/apps/homeownerassociation/models.py
@@ -232,6 +232,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=20)
     fullname = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.email
