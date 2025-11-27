4146c657f90de0727c2fc00dbc6e17553723d540
Koen Schaft <koen@schoenkaft.com>
2025-11-27 11:51:22 +0100

    Add course_date column to HOA Contact model

--- app/apps/homeownerassociation/models.py
+++ app/apps/homeownerassociation/models.py
@@ -234,6 +234,7 @@ class Contact(models.Model):
     phone = models.CharField(max_length=20)
     fullname = models.CharField(max_length=255)
     role = models.CharField(max_length=255)
+    course_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.email
